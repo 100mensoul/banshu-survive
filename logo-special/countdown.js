85b052eaeba62abaed3a647e78732bffb4c94faf
100mensoul <momo2ro1260@gmail.com>
2025-04-16 17:21:45 +0900

    Delete logo-special directory

--- logo-special/countdown.js
+++
@@ -1,5 +0,0 @@
-const releaseDate = new Date('2025-04-25');
-const today = new Date();
-const timeDiff = releaseDate - today;
-const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-document.getElementById("daysLeft").textContent = daysLeft;
