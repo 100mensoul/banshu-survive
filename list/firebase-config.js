de8baa0ce68f6e6bb552b1a9b3046943ffd8dcea
100mensoul <momo2ro1260@gmail.com>
2025-04-21 16:48:15 +0900

    firebase-config.js を作成

---
+++ list/firebase-config.js
@@ -0,0 +1,20 @@
+// /list/firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const projectsRef = ref(db, "projects");
+
+export { projectsRef, push, onChildAdded };
