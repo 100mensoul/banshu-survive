592a8f0b21f4f00666d840c377b6ff11a3ea38d0
100mensoul <momo2ro1260@gmail.com>
2025-05-04 16:07:34 +0900

    Update firebase-config.js

--- list/firebase-config.js
+++ list/firebase-config.js
@@ -1,23 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, push as firebasePush, onChildAdded } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref,
+         push as firebasePush, onChildAdded }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCtDPnYex-KL2hbHAQe5fYSPv9rz9xTa9A",
-  authDomain: "u2memo-36f61.firebaseapp.com",
-  databaseURL: "https://u2memo-36f61-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "u2memo-36f61",
-  storageBucket: "u2memo-36f61.appspot.com",
-  messagingSenderId: "14274931072",
-  appId: "1:14274931072:web:5d9c9026905fdc0b383965"
-};
+const firebaseConfig = { /* ← いま貼ってある JSON で OK */ };
 
-// Firebase 初期化
 const app = initializeApp(firebaseConfig);
 const db  = getDatabase(app);
 
-// Realtime Database の /projects ノード
-export const projectsRef = ref(db, 'projects');
-// データ追加用 push
+export const projectsRef = ref(db, "projects");
 export const push        = firebasePush;
-// 購読用 onChildAdded
 export { onChildAdded };
