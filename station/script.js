77c1644cfe5e854cfa0b34d61803e85021e7557d
100mensoul <momo2ro1260@gmail.com>
2025-05-19 08:37:14 +0900

    script.js を更新

--- station/script.js
+++ station/script.js
@@ -8,11 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     card.innerHTML = `
       <img src="${item.image}" alt="${item.name}">
       <div class="award-title">${item.name} <span>｜${item.award}</span></div>
+      <div class="award-tagline">${item.tagline}</div>
       <div class="award-description">${item.description}</div>
+      <div class="award-featured">看板商品：${item.featured_item}</div>
       <div class="award-links">
         ${item.album ? `<a href="${item.album}" target="_blank">写真アルバム</a>` : ''}
         ${item.link ? `<a href="${item.link}" target="_blank">公式サイト</a>` : ''}
       </div>
+      <div class="comment-placeholder">※ コメント機能は今後追加予定です。</div>
     `;
 
     grid.appendChild(card);
